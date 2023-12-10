754a7ec694a4faca445c40576b680f98712dc670
Sushi34 <100191689+Dak9ta@users.noreply.github.com>
2023-12-10 12:34:49 +0100

    Style the text with upper case and eliminating whitespaces

---
+++ Functions/style.py
@@ -0,0 +1,7 @@
+name = input("What's your name? ")
+
+# Style your text to eliminate white spaces in the left or right of the string and capitalize all the first letters of that string
+name = name.strip()
+name = name.title()
+
+print ("Hello " + name)
