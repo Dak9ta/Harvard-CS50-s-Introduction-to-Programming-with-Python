be9b5cf034fc714408bea694026f7b69756c8ff8
Sushi34 <100191689+Dak9ta@users.noreply.github.com>
2023-12-10 12:35:57 +0100

    Select parts of the string

---
+++ Functions/select-text.py
@@ -0,0 +1,7 @@
+# Minimalize the code
+name = input("What's your name? ").strip().title()
+
+# Select what part of the string you want to display
+first, last = name.split()
+
+print ("Hello " + last)
