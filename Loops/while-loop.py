8d1d55c97109d104ef43396c7a5342469f3d5168
Sushi34 <kianbonci@gmail.com>
2023-12-11 15:41:33 +0100

    Here you will see a controlled while loop

---
+++ Loops/while-loop.py
@@ -0,0 +1,15 @@
+# Here you will see a controlled while loop
+i = 0
+while i < 3:
+    print ("meow")
+    i += 1
+
+'''
+# You can also use this method, but the most used is the one up ^^
+i = 3
+while i != 0:
+    print ("meow")
+    i -= 1
+'''
+
+print()
