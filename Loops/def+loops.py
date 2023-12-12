200423409cc6fbe3f89c2ae01dc5391b19778269
Sushi34 <kianbonci@gmail.com>
2023-12-12 07:51:09 +0100

    This is a loop with custom defined functions

---
+++ Loops/def+loops.py
@@ -0,0 +1,16 @@
+# This is a loop with custom defined functions
+def main():
+    number = get_number()
+    meow(number)
+
+def get_number():
+    while True:
+        n = int(input("What's n? "))
+        if n > 0:
+            return n
+        
+def meow(n):
+    for _ in range(n):
+        print("meow")
+        
+main()
