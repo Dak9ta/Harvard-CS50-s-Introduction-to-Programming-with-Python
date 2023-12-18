6d85cb62161c0bd47833e2c1904841d9d1b42f0b
Sushi34 <kianbonci@gmail.com>
2023-12-18 08:22:04 +0100

    Is like an input, but you have to enter your name after the program name: "python program.py name"

--- Libraries/argv.py
+++ Libraries/argv.py
@@ -1,3 +1,17 @@
 import sys
 
-print("hello, my name is", sys.argv[1]) # Is like an input, but you have to enter your name after the program name: "python program.py name"
+# Check for errors
+if len(sys.argv) < 2:
+    sys.exit("Too few arguments")
+elif len(sys.argv) > 2:
+    sys.exit("Too many arguments")
+
+# Print name line
+print("hello, my name is", sys.argv[1])
+    
+# Try to prevent the error message
+# try:
+#     print("hello, my name is", sys.argv[1]) # Is like an input, but you have to enter your name after the program name: "python program.py name"
+    
+# except IndexError:
+#     print("You have to enter your name after the program name")
