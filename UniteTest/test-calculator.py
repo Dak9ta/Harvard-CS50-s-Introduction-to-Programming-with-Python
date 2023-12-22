7ba0303dde57a20a3c49cfc23657341bc7ee54e4
Sushi34 <kianbonci@gmail.com>
2023-12-22 07:50:43 +0100

    I changed some thing to see if the code worked proprietly

--- UniteTest/test-calculator.py
+++ UniteTest/test-calculator.py
@@ -3,8 +3,9 @@ from calculator import square
 def main():
     test_square()
 
+# I've made it this way, because it easier to test errors like this
 def test_square():
-    for n in range(0,100000):
+    for n in range(-10,10):
         if square(n) != n*n:
             print(f"{n} square was not {n*n}")
         n += 1
