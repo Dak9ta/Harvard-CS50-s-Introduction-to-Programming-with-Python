00ca3536e18bdbc86a0aa3bc67dc5da76b8bb9b1
Sushi34 <kianbonci@gmail.com>
2023-12-23 09:10:47 +0100

    this is a little bit simplefied with lambda

--- File I-O/csv/sort-house-&-name.py
+++ File I-O/csv/sort-house-&-name.py
@@ -6,12 +6,6 @@ with open("File I-O/csv/students.csv") as file:
         student = {"name": name, 
                    "house": house}
         students.append(student)
-        
-def get_name(student):
-    return student["name"]
-
-def get_house(student):
-    return student["house"]
-        
-for student in sorted(students, key=get_house):
-    print(f"{student['name']}, {student['house']}")
+       
+for student in sorted(students, key=lambda student: student["name"]):
+    print(f"{student['name']} is in {student['house']}")
